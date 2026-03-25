18c1973e2fbf1b8d6be665d8e3d8b5090461e5d5
maybe-adamant <maybe.adamant@gmail.com>
2026-03-25 16:23:09 -0400

    changing discovery flag from bool to string. preparing to moving core to a framework

--- src/main.lua
+++ src/main.lua
@@ -27,7 +27,7 @@ public.definition = {
     tooltip  = "Fixes OCast moves not counting as cast damage.",
     default  = true,
     dataMutation = true,
-    modpackModule = true,
+    modpack = "h2-modpack",
 }
 
 -- =============================================================================
